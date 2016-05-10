786d942ed295088f2b53f9671ec1a059a7b9df24
Fernando Boose <fernandoboose@Fernandos-MacBook-Pro.local>
2016-05-10 01:06:23 -0300

    hiding bahia flag on ranking page, because it overlaps the content

--- web/js/ranking.js
+++ web/js/ranking.js
@@ -5,5 +5,7 @@ $('document').ready(function(){
 			$('.game-ranking-wrap').html('<div class = "ajax-loader-wrap"><img class = "ajax-loader" src = "../img/ajax_loader.gif" /></div>');
             $('.game-ranking-wrap').load('/ranking/'+gameId);
 		}
-	})
+	});
+
+	$('.bahia-flag').hide();
 })
