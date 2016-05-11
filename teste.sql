ac0f6f2badc25cce486092fbb9bfdda8659e5d84
Fernando Boose <fernandoboose@Fernandos-MacBook-Pro.local>
2016-05-11 01:20:53 -0300

    sql

--- teste.sql
+++ teste.sql
@@ -1,19 +1,3 @@
--- INSERT INTO `question` (`id`, `desc`) VALUES ('1', 'Porque a galinha atravessou a rua?'), ('2', 'Quanto é 1 + 1?');
-
-
--- INSERT INTO `question_option` (`id`, `desc`, `correct_answer`, `question_id`) VALUES 
--- ('1', 'Pra chegar ao outro lado', '1', '1'), 
--- ('2', 'Pois ela estava afim', '0', '1'), 
--- ('3', 'Não sei cara', '0', '1'), 
--- ('4', 'Tanto faz, na real', '0', '1');
-
--- INSERT INTO `question_option` (`id`, `desc`, `correct_answer`, `question_id`) VALUES 
--- ('5', '1 + 1 = 9', '0', '2'), 
--- ('6', '1 + 1 = 3', '0', '2'), 
--- ('7', '1 + 1 = 2', '1', '2'), 
--- ('8', '1 + 1 = 45', '0', '2');
-
-
 -- Pergunta 1
 INSERT INTO `question` (`id`, `desc`) VALUES ('1', 'Um dos itens abaixo não pertence ao grupo.');
 
